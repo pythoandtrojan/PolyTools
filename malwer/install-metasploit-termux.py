d2cfa4be2d2ce4d1bf934b733d2a67bec7c44786
pythoandtrojan <erik.silva40@aluno.educa.go.gov.br>
2025-10-14 18:28:49 -0300

    Update install-metasploit-termux.py

--- malwer/install-metasploit-termux.py
+++ malwer/install-metasploit-termux.py
@@ -1,5 +1,6 @@
 import os 
 
 os.system('git clone https://github.com/gushmazuko/metasploit_in_termux.git')
+os.chdir("metasploit_in_termux")
 os.system('chmod +x  metasploit.sh')
 os.system('bash ./install.sh')
